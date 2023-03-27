8abab4ed92f8bb381ef209628de2c4cc27b4622e
Xaivntaaj Her <dn_her@yahoo.com>
2023-03-27 11:15:36 -0500

    updated

--- day_trip_gen.py
+++ day_trip_gen.py
@@ -5,64 +5,24 @@ resturants = ['Olive Garden', 'Red Lobster', 'Hot N Juicy', 'Five Guys', 'Wingst
 transportations = ['Car', 'Train', 'Airplane', 'Bus', 'Bike']
 entertainments = ['Movie', 'Comedy Show', 'Skydiving', 'Bungee Jumping', 'Concert']
 
-def display_destination(list_destinations, list_resturants, list_transportations, list_entertainments):
-    for place in list_destinations:
-        print(place)
-    for rest in list_resturants:
-        print(rest)
-    for travel in list_transportations:
-        print(travel)
-    for ent in list_entertainments:
-        print(ent)
-    
-
-# def display_destination(list_destinations): 
-#     for place in list_destinations:
-#         print(place)
-
-# def display_resturant(list_resturants):
-#     for rest in list_resturants:
-#         print(rest)
-
-# def display_transportation(list_transportations):
-#     for travel in list_transportations:
-#         print(travel)
-
-# def display_entertainment(list_entertainments):
-#     for ent in list_entertainments:
-#         print(ent)
-
-def display_new_destination(list_new_destinations, list_new_resturants, list_new_transportations, list_new_entertainments):
-    for new_place in list_new_destinations:
-        print(new_place)
-    for new_rest in list_new_resturants:
-        print(new_rest)
-    for new_travel in list_new_transportations:
-        print(new_travel)
-    for new_ent in list_new_entertainments:
-        print(new_ent)   
-
-# def display_new_destination(list_new_destinations):
-#     for new_place in list_new_destinations:
-#         print(new_place)
-
-# def display_new_resturant(list_new_resturants):
-#     for new_rest in list_new_resturants:
-#         print(new_rest)
-
-# def display_new_transportation(list_new_transportations):
-#     for new_travel in list_new_transportations:
-#         print(new_travel)
-
-# def display_new_entertainment(list_new_entertainments):
-#     for new_ent in list_new_entertainments:
-#         print(new_ent)
-
 random_new_destination = random.choice(destinations)
 random_new_resturant = random.choice(resturants)
 random_new_transportation = random.choice(transportations)
 random_new_entertainment = random.choice(entertainments)
 
+# def display_day_trip(destinations, resturants, transportations, entertainments):
+#     random_destination = random.choice(destinations)
+#     print(f'Destination: {random_destination}')
+#     random_resturant = random.choice(resturants)
+#     print(f'Resturant: {random_resturant}')
+#     random_transportation = random.choice(transportations)
+#     print(f'Transportation: {random_transportation}')
+#     random_entertainment = random.choice(entertainments)
+#     print(f'Eyntertainment: {random_entertainment}')
+#     return (random_destination, random_resturant, random_transportation, random_entertainment)
+    
+
+# display_day_trip(destinations,resturants,transportations,entertainments)
 
 random_destination = random.choice(destinations)
 print(f'Destination: {random_destination}')
@@ -73,8 +33,11 @@ print(f'Transportation: {random_transportation}')
 random_entertainment = random.choice(entertainments)
 print(f'Eyntertainment: {random_entertainment}')
 
+
 satisfy = input('Are you satisfied with your trip? Y or N ')
 print(satisfy)
+
+# def print_day_trip()
 
 if satisfy == 'Y':
     print(f'Have fun and enjoy your trip!')
@@ -111,11 +74,9 @@ elif satisfy == 'N':
     if new_satify == "Y":
         print()
     elif new_satify == 'N':
-        print(not_satify)
-        # print(f'Destination: {random_new_destination}')
-        # print(f'Resturant: {random_new_resturant}')
-        # print(f'Transportation: {random_new_transportation}')
-        # print(f'Entertainment: {random_new_entertainment}')    
+        print()
+        
+      
 
             
     
